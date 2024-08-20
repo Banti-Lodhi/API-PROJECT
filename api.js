db2a1458c71f4347d1cccf01e888300ea84600b8
Banti-Lodhi <166380570+Banti-Lodhi@users.noreply.github.com>
2024-08-20 17:32:36 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,9 @@
+let jsonData = {
+  "fact":"Every year, nearly four million cats are eaten in Asia.","length":55
+}
+console.log(jsonData.fact);
+
+let student = {
+  name: "Sharada",
+  age : 26
+}
